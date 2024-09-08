c373c18977720355361d7683bfdf2e149bb7d4c8
Emanuel <emavazquez954@gmail.com>
2024-09-08 17:29:29 -0600

    Calculadora Terminada

--- src/main/java/com/example/tap2024b/vistas/Calculadora.java
+++ src/main/java/com/example/tap2024b/vistas/Calculadora.java
@@ -17,17 +17,21 @@ public class Calculadora extends Stage {
     private Scene escena;
     private String[] strTeclas = {"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
     private Button btnClear;
+    String numero="", operador="";
+    double n1=0,n2=0, total=0;
+
     private void CrearUI(){
         arrBtns =  new Button[4][4];
-        txtPantalla = new TextField("0");
+        txtPantalla = new TextField();
         txtPantalla.setAlignment(Pos.CENTER_RIGHT);
         txtPantalla.setEditable(false);
         gdpTeclado = new GridPane();
         CrearTeclado();
         btnClear = new Button("CE");
-        btnClear.setId("font-button");
+        btnClear.setPrefSize(75,75);
+        btnClear.setId("color-btnclear");
         vbox = new VBox(txtPantalla, gdpTeclado, btnClear);
-        escena = new Scene(vbox,300, 300);
+        escena = new Scene(vbox,300, 375);
     }
 
     private void CrearTeclado(){
@@ -41,6 +45,14 @@ public class Calculadora extends Stage {
                 int finalJ = j;
                 arrBtns[j][i].setOnAction(event -> DetectarTecla(strTeclas[4* finalI + finalJ]));
                 gdpTeclado.add(arrBtns[j][i],j,i);
+
+                if (arrBtns[j][i].getText().equals("+") || arrBtns[j][i].getText().equals("-") || arrBtns[j][i].getText().equals("*")
+                        || arrBtns[j][i].getText().equals("/") || arrBtns[j][i].getText().equals("=")) {
+                    arrBtns[j][i].setId("color-operador");
+                } else {
+                    arrBtns[j][i].setId("color-numeros");
+                }
+
             }
         }
     }
@@ -55,17 +67,198 @@ public class Calculadora extends Stage {
 
     private void DetectarTecla(String tecla) {
 
-        txtPantalla.appendText(tecla);
-        String numero1="", numero2="";
-        String operador="";
-        if (txtPantalla.getText()!="+" || txtPantalla.getText()!="-" || txtPantalla.getText()!="*" ||
-                txtPantalla.getText()!="/" || txtPantalla.getText()!="."){
-            numero1=txtPantalla.getText();
-            System.out.println("El numero es: "+numero1);
-        } else {
-            operador = txtPantalla.getText();
-            System.out.println("el operador es: "+operador);
+        switch (tecla) {
+            case "9":
+                numero += "9";
+                txtPantalla.appendText("9");
+                break;
+            case "8":
+                numero += "8";
+                txtPantalla.appendText("8");
+                break;
+            case "7":
+                numero += "7";
+                txtPantalla.appendText("7");
+                break;
+            case "6":
+                numero += "6";
+                txtPantalla.appendText("6");
+                break;
+            case "5":
+                numero += "5";
+                txtPantalla.appendText("5");
+                break;
+            case "4":
+                numero += "4";
+                txtPantalla.appendText("4");
+                break;
+            case "3":
+                numero += "3";
+                txtPantalla.appendText("3");
+                break;
+            case "2":
+                numero += "2";
+                txtPantalla.appendText("2");
+                break;
+            case "1":
+                numero += "1";
+                txtPantalla.appendText("1");
+                break;
+            case "0":
+                numero += "0";
+                txtPantalla.appendText("0");
+                break;
+            case ".":
+                // Validar si es el primer caracter
+                if (txtPantalla.getText().isEmpty()) {
+                    // No permitir agregar un punto como primer carácter
+                    txtPantalla.appendText("NO SE PUEDE EMPEZAR CON UN PUNTO");
+                    DesactivarBotones();
+                } else {
+                    numero += ".";
+                    txtPantalla.appendText(".");
+                }
+                break;
+
+            //Operadores
+            case "+":
+                if (txtPantalla.getText().isEmpty()) {
+                    // No permitir agregar un punto como primer carácter
+                    txtPantalla.appendText("ERROR");
+                    DesactivarBotones();
+                } else {
+                    n1=Double.parseDouble(txtPantalla.getText());
+                    operador="+";
+                    txtPantalla.clear();
+
+                    arrBtns[3][0].setDisable(true);
+                    arrBtns[3][1].setDisable(true);
+                    arrBtns[3][2].setDisable(true);
+                    arrBtns[3][3].setDisable(true);
+                }
+                break;
+            case "-":
+                if (txtPantalla.getText().isEmpty()) {
+                    // No permitir agregar un punto como primer carácter
+                    txtPantalla.appendText("ERROR");
+                    DesactivarBotones();
+                } else {
+                    n1=Double.parseDouble(txtPantalla.getText());
+                    operador="-";
+                    txtPantalla.clear();
+
+                    arrBtns[3][0].setDisable(true);
+                    arrBtns[3][1].setDisable(true);
+                    arrBtns[3][2].setDisable(true);
+                    arrBtns[3][3].setDisable(true);
+                }
+                break;
+            case "*":
+                if (txtPantalla.getText().isEmpty()) {
+                    // No permitir agregar un punto como primer carácter
+                    txtPantalla.appendText("ERROR");
+                    DesactivarBotones();
+                } else {
+                    n1=Double.parseDouble(txtPantalla.getText());
+                    operador="*";
+                    txtPantalla.clear();
+
+                    arrBtns[3][0].setDisable(true);
+                    arrBtns[3][1].setDisable(true);
+                    arrBtns[3][2].setDisable(true);
+                    arrBtns[3][3].setDisable(true);
+                }
+                break;
+            case "/":
+                if (txtPantalla.getText().isEmpty()) {
+                    // No permitir agregar un punto como primer carácter
+                    txtPantalla.appendText("ERROR");
+                    DesactivarBotones();
+                } else {
+                    n1=Double.parseDouble(txtPantalla.getText());
+                    operador="/";
+                    txtPantalla.clear();
+
+                    arrBtns[3][0].setDisable(true);
+                    arrBtns[3][1].setDisable(true);
+                    arrBtns[3][2].setDisable(true);
+                    arrBtns[3][3].setDisable(true);
+                }
+                break;
+
+            case "=":
+                if (txtPantalla.getText().isEmpty()) {
+                    // No permitir agregar un punto como primer carácter
+                    txtPantalla.appendText("ERROR COLOCA NUMEROS");
+                    DesactivarBotones();
+                } else {
+                    if (operador.equals("+")){
+                        n2=Double.parseDouble(txtPantalla.getText());
+                        txtPantalla.clear();
+                        total = n1+n2;
+                        txtPantalla.setText(String.valueOf(total));
+                        DesactivarBotones();
+                    } else if (operador.equals("-")) {
+                        n2=Double.parseDouble(txtPantalla.getText());
+                        txtPantalla.clear();
+                        total = n1-n2;
+                        txtPantalla.setText(String.valueOf(total));
+                        DesactivarBotones();
+                    } else if (operador.equals("*")) {
+                        n2=Double.parseDouble(txtPantalla.getText());
+                        txtPantalla.clear();
+                        total = n1*n2;
+                        txtPantalla.setText(String.valueOf(total));
+                        DesactivarBotones();
+                    } else if (operador.equals("/")) {
+                        n2=Double.parseDouble(txtPantalla.getText());
+                        if (n2 == 0) {
+                            txtPantalla.setText("NO SE PUEDE DIVIDIR ENTRE 0");
+                            DesactivarBotones();
+                            break;
+                        } else {
+                            txtPantalla.clear();
+                            total = n1/n2;
+                            txtPantalla.setText(String.valueOf(total));
+                            DesactivarBotones();
+                        }
+
+                    }
+
+                }
+                break;
+
         }
+        btnClear.setOnAction(event -> {
+            numero="";
+            n1=0;
+            n2=0;
+            total=0;
+            txtPantalla.clear();
+            ActivarBotones();
+        });
+        System.out.println("Numero 1: "+n1);
+        System.out.println("Operador: "+operador);
+        System.out.println("Numero 2: "+n2);
+        System.out.println("El total es: "+total);
 
     }
+
+    private void DesactivarBotones() {
+        for (int i = 0; i < arrBtns.length; i++) {
+            for (int j = 0; j < arrBtns[i].length; j++) {
+                arrBtns[j][i].setDisable(true);
+            }
+        }
+    }
+    private void ActivarBotones() {
+        for (int i = 0; i < arrBtns.length; i++) {
+            for (int j = 0; j < arrBtns[i].length; j++) {
+                arrBtns[j][i].setDisable(false);
+            }
+        }
+    }
+
+
+
 }
